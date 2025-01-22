01372b319b59eee7a4a939c540878e2882d8fa20
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-01-22 13:03:58 -0700

    random num function

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # Ms.LaRose, and Vienna github test code
-
+import random
 def hello_world(name):
     return f"Hello {name}!"
+
+def num_generator():
+    num = random.randint(1,100)
+    return num
 
 
 def main():
     print("Welcome to my program. Please enter your name:\n")
     name=input()
-
+    print(num_generator())
 main()
